0f78f541de9dcb625b2413d19c9e0da2da1bfafe
SebSutkowy <13sutkowys@student.smchull.org>
2025-02-03 10:01:44 +0000

    Added a health bar

--- SchoolFighter/GameScene.cs
+++ SchoolFighter/GameScene.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using System.Diagnostics;
 
 namespace SchoolFighter
 {
@@ -8,26 +7,34 @@ namespace SchoolFighter
     {
         Character _player { get; set; }
         Character _enemy { get; set; }
+        HealthBar _playerHealthBar { get; set; }
+        HealthBar _enemyHealthBar { get; set; }
+        
         public GameScene() { }
 
         public override void LoadContent()
         {
             _player = new Character(Globals._playerTexture, Vector2.Zero, Color.White, 300.0f, Vector2.Zero, 100, 10, 1);
             _enemy = new Character(Globals._playerTexture, new Vector2(800, 0), Color.White, 300.0f, Vector2.Zero, 100, 10, 2);
+            _playerHealthBar = new HealthBar(_player.Health, Vector2.Zero, new Vector2(Globals.winWidth * 2 / 5, Globals.winHeight / 12), Directions.Left);
+            _enemyHealthBar = new HealthBar(_enemy.Health, new Vector2(Globals.winWidth * 3/5, 0), new Vector2(Globals.winWidth * 2 / 5, Globals.winHeight / 12), Directions.Right);
         }
 
         public override void Update() 
         {
             _player.Update();
             _enemy.Update(false);
+            _playerHealthBar.Update(_player.Health);
+            _enemyHealthBar.Update(_enemy.Health);
             HitboxManager.Update();
-            
         }
 
         public override void Draw(SpriteBatch spriteBatch)
         {
             _player.Draw(spriteBatch);
             _enemy.Draw(spriteBatch);
+            _playerHealthBar.Draw(spriteBatch);
+            _enemyHealthBar.Draw(spriteBatch);
             HitboxManager.Draw(spriteBatch);
         }
     }
---
+++ SchoolFighter/HealthBar.cs
@@ -0,0 +1,44 @@
+﻿
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System.Diagnostics;
+
+namespace SchoolFighter
+{
+    internal class HealthBar
+    {
+        public int TotalHealth { get; set; }
+        public Vector2 Position { get; set; }
+        public Vector2 Size { get; set; }
+        public Directions Direction { get; set; }
+        public Rectangle healthBar;
+
+
+        public HealthBar(int totalHealth, Vector2 position, Vector2 size, Directions direction) 
+        { 
+            TotalHealth = totalHealth;
+            Position = position;
+            Size = size;
+            Direction = direction;
+        }
+
+        public void Update(int currentHealth)
+        {
+            int x = 0;
+            if (Direction == Directions.Left)
+            {
+                x = (int)(Size.X * (1 - (float)currentHealth / (float)TotalHealth) + Position.X);
+            }
+            else if (Direction == Directions.Right)
+            {
+                x = (int)Position.X;
+            }
+            healthBar = new Rectangle(x, (int)Position.Y, (int)(Size.X * ((float)currentHealth / (float)TotalHealth)), (int)Size.Y);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Globals._playerTexture, healthBar, Color.Green);
+        }
+    }  
+}
